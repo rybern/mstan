1f8c28065765db284804e2eb2e6f3f6796bf4ea4
Ryan Bernstein <ryanbernstein1@gmail.com>
2021-04-22 14:03:31 -0400

    Initial mockup

---
+++ src/Mockup.hs
@@ -0,0 +1,149 @@
+{-# LANGUAGE EmptyDataDeriving #-}
+import           Data.Set                       ( Set )
+import qualified Data.Set                      as Set
+import           Data.Map                       ( Map )
+import qualified Data.Map                      as Map
+import           Data.Maybe
+import           Data.List
+
+type Symbol = String
+
+-- Stan-ish types
+data Type
+data Expr
+data Code deriving (Eq, Ord)
+data Param deriving (Eq, Ord)
+
+-- Module system types
+type SigName = Symbol
+type ImplName = Symbol
+type InstanceName = Symbol
+data Reference deriving (Eq, Ord)
+
+-- Program types
+
+-- Constraint:
+-- for all `implementations`, the body follows the matching signature in `signatures`
+data ModularProgram = ModularProgram {
+    signatures :: Set (Type, SigName) -- Constraints: Unique SigName
+  , implementations :: Set ModuleImplementation
+  , topBody :: Code
+  , topParams :: Set Param
+  }
+
+data ModuleImplementation = ModuleImplementation {
+  -- explicit list of args
+    implBody :: [Expr] -> Code
+  , implSignature :: SigName
+  , implParams :: Set Param
+  , implName :: ImplName
+  }
+
+data ConcreteProgram = ConcreteProgram {
+    concreteBody :: Code
+  , concreteParams :: Set Param
+  }
+
+-- Constraint:
+-- The selection map is total for all signatures in the program
+applyModuleSelections
+    :: ModularProgram -> Map SigName ImplName -> ConcreteProgram
+applyModuleSelections program selectionNames = ConcreteProgram
+    { concreteBody   = applyModuleReferences appliedSigImplementations
+                                             (topBody program)
+    , concreteParams = Set.union (topParams program) moduleParams
+    }
+  where
+    selections = Map.map
+        (\name ->
+            fromJust
+                (find (\impl -> name == implName impl) (implementations program)
+                )
+        )
+        selectionNames
+    (sigImplementations, moduleParams) =
+        selectImplementations selections (implementations program)
+    applyModules = applyModuleReferences sigImplementations
+    applyOrderSigs = (topologicallyOrderSignatures sigImplementations)
+    appliedSigImplementations = foldl
+        (\impls sigName -> Map.update
+            (\_ -> Just
+                (\args -> applyModuleReferences
+                    impls
+                    ((fromJust (Map.lookup sigName impls)) args)
+                )
+            )
+            sigName
+            impls
+        )
+        sigImplementations
+        applyOrderSigs
+
+selectImplementations
+    :: Map SigName ModuleImplementation
+    -> Set ModuleImplementation
+    -> (Map SigName ([Expr] -> Code), Set Param)
+selectImplementations =
+  undefined
+
+instanceInitializers
+    :: Map SigName ([Expr] -> Code) -> Code -> Map InstanceName Code
+instanceInitializers sigImpls code =
+  undefined inits
+    where inits = moduleInstances code
+
+applyModuleReferences :: Map SigName ([Expr] -> Code) -> Code -> Code
+applyModuleReferences sigImpls code = Set.fold applyReference
+                                               code
+                                               translatedReferences
+  where
+    instances            = instanceInitializers sigImpls code
+    references           = moduleReferences code
+    translatedReferences = Set.map
+        (\(instanceName, reference) ->
+            (fromJust (Map.lookup instanceName instances), reference)
+        )
+        references
+
+moduleReferences :: Code -> Set (InstanceName, Reference)
+moduleReferences =
+  undefined
+
+moduleInstances :: Code -> Set (SigName, InstanceName, [Expr])
+moduleInstances = undefined
+
+applyReference :: (Code, Reference) -> Code -> Code
+applyReference = undefined
+
+topologicallyOrderSignatures :: Map SigName ([Expr] -> Code) -> [SigName]
+topologicallyOrderSignatures =
+  undefined
+
+
+-- Nouns:
+
+-- Module signature
+type ModuleSignature = (Type, SigName)
+-- Module instantiation
+type ModuleInstantiation = (SigName, InstanceName, [Expr])
+-- Module implementation
+type ModuleImplementation' = ModuleImplementation
+-- Modular program
+type ModularProgram' = ModularProgram
+-- Concrete program/model
+type ConcreteProgram' = ConcreteProgram
+-- Parameter set
+type ParameterSet = Set Param
+-- Module application
+type ModuleApplication = ModularProgram -> Map SigName ImplName -> ConcreteProgram
+-- Module reference
+type ModuleReference = Reference
+-- Module selection
+type ModuleSelection = (SigName, ImplName)
+
+golf = ModularProgram {
+    signatures :: Set (Type, SigName) -- Constraints: Unique SigName
+  , implementations :: Set ModuleImplementation
+  , topBody :: Code
+  , topParams :: Set Param
+  }
