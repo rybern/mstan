cf1e23556972d6b9a3122fceba6a0c18a86990a4
Ryan Bernstein <ryanbernstein1@gmail.com>
2022-01-05 17:15:50 -0500

    Custom versions of graph join and graph cartesian product to avoid tree issues

--- lib/ModelGraph.hs
+++ lib/ModelGraph.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
 {-# LANGUAGE TupleSections     #-}
@@ -10,9 +11,8 @@ module ModelGraph (
   , modelGraphviz
   ) where
 
-import           Algebra.Graph
-import           Algebra.Graph.Undirected (neighbours, toUndirected)
-import           Data.List                (foldl1')
+import           Data.List                (foldl')
+import           Data.Maybe
 import qualified Data.Map                 as Map
 import           Data.Set                 (Set)
 import qualified Data.Set                 as Set
@@ -21,33 +21,57 @@ import qualified Data.Text                as Text
 import           Data.Fix                 (refold)
 import           Data.MemoUgly
 
+
 import           Graphviz
 import           Types
 import           ModuleTree
 
+-- TODO:
+  -- At sigs, custom graph join necessary. Union graphs but manually add edges between
+  -- At impls, nodes can duplicate. Join identical nodes.
+
+type SelectionGraph = (Set Selection, Set (Selection, Selection, ModuleDelta))
+
+selectionDelta :: (Selection, Selection) -> Maybe (Selection, Selection, ModuleDelta)
+selectionDelta (a, b) =
+  (\case
+    [delta] -> Just (a, b, delta)
+    _ -> Nothing)
+  . filter (\(ModuleDelta _ i1 i2) -> i1 /= i2)
+  . Map.elems
+  $ Map.intersectionWithKey (\(SigName sig) (ImplName impl1) (ImplName impl2) ->
+                                    ModuleDelta sig impl1 impl2) a b
+
+setMapMaybe :: (Ord b) => (a -> Maybe b) -> Set a -> Set b
+setMapMaybe f = Set.fromList . mapMaybe f . Set.toList
+
 -- Build a node's graph given its subtrees' graphs; for use in `refold` or `unfold`
--- Using algebraic graph construction, see https://hackage.haskell.org/package/algebraic-graphs-0.6
-joinGraphs :: ModuleBranch (Graph Selection) -> Graph Selection
-joinGraphs (SigBranch _ implGraphs) = foldl1' graphJoin implGraphs
+joinGraphs :: ModuleBranch SelectionGraph -> SelectionGraph
+joinGraphs (SigBranch _ implGraphs) = foldl' join (Set.empty, Set.empty) implGraphs
   -- Signature nodes combine children by graph join:
   --   https://en.wikipedia.org/wiki/Graph_operations
-  where graphJoin = connect
-joinGraphs (ImplBranch impl []) = Vertex impl
-joinGraphs (ImplBranch impl sigGraphs) = (impl <>) <$> foldl1' cartesianProduct sigGraphs
+  where join (n1, e1) (n2, e2) =
+          (n1 <> n2,
+           e1 <> e2 <> setMapMaybe selectionDelta (Set.cartesianProduct n1 n2))
+joinGraphs (ImplBranch impl sigGraphs) = foldl' product (Set.singleton impl, Set.empty) sigGraphs
   -- Implementation nodes combine children by Cartesian graph product:
   --   https://en.wikipedia.org/wiki/Cartesian_product_of_graphs
-  where cartesianProduct g1 g2 = uncurry (<>) <$> box g1 g2
+  where product (n1, e1) (n2, e2) =
+          ( Set.map (uncurry (<>)) (Set.cartesianProduct n1 n2)
+          , Set.map (\((a, b, d), s) -> (a<>s, b<>s, d))
+            (Set.cartesianProduct e1 n2 <> Set.cartesianProduct e2 n1)
+          )
 
 -- Grow a module tree from Root with `growTree`, fold into a graph with `joinGraphs` (hylomorphism)
 -- Use memoization because modules may not actually form a tree, so sub"trees" may be repeated
-modelGraph :: ModularProgram -> Graph Selection
+modelGraph :: ModularProgram -> SelectionGraph
 modelGraph p = refold (memo joinGraphs) (growTree p) (Impl Root)
 
 allSelections :: ModularProgram -> Set Selection
-allSelections = vertexSet . modelGraph
+allSelections = fst . modelGraph
 
 modelNeighbors :: ModularProgram -> Selection -> Set Selection
-modelNeighbors p s = neighbours s . toUndirected $ modelGraph p
+modelNeighbors p s = undefined
 
 -- Faster than `Set.head . allSelections`
 firstSelection :: ModularProgram -> Selection
@@ -66,22 +90,10 @@ modelGraphviz p = modelGraphToDot (decoratedModelGraph p)
 decoratedModelGraph :: ModularProgram -> ModelGraph
 decoratedModelGraph p = ModelGraph allModels modelEdges
   where
-    graph = modelGraph p
+    (nodes, edges) = modelGraph p
     toNode = ModelNode . showSel
-    allModels = Set.map toNode (vertexSet graph)
-    modelEdges = map edgeDelta . Set.toList $ edgeSet graph
-
-    selectionDeltas :: Selection -> Selection -> [ModuleDelta]
-    selectionDeltas a b =
-        filter (\(ModuleDelta _ i1 i2) -> i1 /= i2)
-      . Map.elems
-      $ Map.intersectionWithKey (\(SigName sig) (ImplName impl1) (ImplName impl2) ->
-                                    ModuleDelta sig impl1 impl2) a b
-
-    edgeDelta :: (Selection, Selection) -> (ModelNode, ModelNode, ModuleDelta)
-    edgeDelta (a, b) = case selectionDeltas a b of
-      [delta] -> (toNode a, toNode b, delta)
-      ds -> error $ "Internal issue: neighboring selections differ by !=1 key. " ++ show ds
+    allModels = Set.map toNode nodes
+    modelEdges = map (\(a, b, d) -> (toNode a, toNode b, d)) . Set.toList $ edges
 
 showSel :: Selection -> Text
 showSel = Text.unlines
